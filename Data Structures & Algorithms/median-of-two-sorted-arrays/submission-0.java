7a297213a79727b059faf7eb82250d9a88631aac
Varrray <116029848+Varrray@users.noreply.github.com>
2026-09-05 21:44:04 -0700

    Add: median-of-two-sorted-arrays - submission-0

---
+++ Data Structures & Algorithms/median-of-two-sorted-arrays/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int n1=nums1.length(),n2=nums2.length();
+        List<Integer> ans=new ArrayList<>();
+        int i=0,j=0;
+        while(i<n1 && j<n2){
+            if(nums1[i]<nums2[j]){ans.add(nums1[i]); i++;}
+            if(nums1[i]>nums2[j])(ans.add(nums[j]); j++;)
+
+
+        }
+        while()
+
+        
+    }
+}
